7eb06cca7d021b16ef07e9d7bf1b41099b7ab818
joanjib <joan@iglesias.com>
2023-03-14 15:12:02 +0100

    changed the calico setup, for a manifest

--- kubernetes/08-calico-network.sh
+++ kubernetes/08-calico-network.sh
@@ -1,4 +1,3 @@
 # calico from the master
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/custom-resources.yaml -O
-kubectl create -f custom-resources.yaml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml -O
+kubectl apply -f calico.yaml
